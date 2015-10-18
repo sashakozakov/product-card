f2ef9e0107de404bd53a031e0896f7e5688061cf
sashakozakov <sashakozakov2293@gmail.com>
2015-10-19 00:24:03 +0300

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
-;(function(){
+;(function($){
+	$(document).ready(function(){
+		var slider = $('.ba-slider');
 
-})();
+		//slider init
+		slider.slick({
+			dots: true
+		});
+
+		//qty up & down
+		var qtyField = $('.ba-product__qty-field'),
+			qtyUp = $('.ba-product__qty-up'),
+			qtyDown = $('.ba-product__qty-down');
+
+		qtyUp.on('click', function(event){
+			event.preventDefault();
+			var oldVal = qtyField.val();
+			qtyField.val(++oldVal);
+		});
+
+		qtyDown.on('click', function(event){
+			event.preventDefault();
+			var oldVal = qtyField.val();
+			qtyField.val(--oldVal === 0 ? 1 : oldVal);
+		});
+	});
+})(jQuery);
